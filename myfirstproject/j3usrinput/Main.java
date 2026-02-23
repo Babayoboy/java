c68ae785b5434a12b600c3d1ef0782edd7b4312f
Ansh joshi <106421634+Babayoboy@users.noreply.github.com>
2026-02-23 18:29:57 +0530

    Update Main.java

---
+++ myfirstproject/j3usrinput/Main.java
@@ -0,0 +1,26 @@
+package j3usrinput;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        double widht;
+        double length;
+        double area;
+        
+        // Calculating area of a rectangle
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter widht: ");
+        widht = scanner.nextDouble();
+        
+        System.out.print("Enter length: ");
+        length = scanner.nextDouble();
+
+        area = widht * length;
+
+        System.out.println("The area is: " + area + "cm^2");
+
+    }
+}
