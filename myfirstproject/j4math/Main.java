8fc1c7d2dd8a4fadf6fa337a09068da34e96eab4
Ansh joshi <106421634+Babayoboy@users.noreply.github.com>
2026-02-25 18:52:34 +0530

    Update Main.java

--- myfirstproject/j4math/Main.java
+++ myfirstproject/j4math/Main.java
@@ -1,7 +1,41 @@
 package myfirstproject.j4math;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // // Hypotinus
+        // double a;
+        // double b;
+        // double c;
+
+
+        // System.out.print("length of side a: ");
+        // a = scanner.nextInt();
+        // System.out.print("legth of side b: ");
+        // b = scanner.nextInt();
+
+        // c = Math.sqrt(((a*a) + (b*b)));
+
+        // System.out.println("The value of side c is " + c + "cm");
         
+        // Circle
+        double radius;
+        double circumference;
+        double area;
+        double volume;
+        
+        System.out.print("Enter the radius: ");
+        radius = scanner.nextDouble();
+
+        circumference = 2 * Math.PI * radius;
+        area = Math.PI * Math.pow(radius, 2);
+        volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+
+        System.out.printf("Circumference of the circle with radius of " + radius + " is %.1f" + circumference + "cm\n");
+        System.out.printf("Area of the circle with radius of " + radius + " is %.1f" + area + "cm²\n");
+        System.out.printf("Volume of the circle with radius of " + radius + " is %.1f" + volume + "cm³\n");
+        scanner.close();
     }
 }
