9a62eabf6e90c9b88d7afa5fb2b24165c166ee6e
Ansh joshi <106421634+Babayoboy@users.noreply.github.com>
2026-02-26 17:00:08 +0530

    Create Main.java

---
+++ projects/compoundIntrestCalculator/Main.java
@@ -0,0 +1,25 @@
+package projects.compoundIntrestCalculator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        double amount;
+        double principal;
+        double rate;
+        double noOfCompounded;
+        double noOfYears;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the Principal amount: ");
+        principal = scanner.nextDouble();
+        System.out.print("Enter the rate of intrest: ");
+        rate = scanner.nextDouble();
+        System.out.print("Enter the number of times interest is compounded per year: ");
+        noOfCompounded = scanner.nextDouble();
+        System.out.print("Enter the number of years the money is invested or borrowed: ");
+        noOfYears = scanner.nextDouble();
+        amount = principal * (Math.pow(1+(rate/100/noOfCompounded), (noOfCompounded * noOfYears)));
+        System.out.printf("The Total Amout after compound intrest is %.2f\n", amount);
+        scanner.close();
+    }
+}
