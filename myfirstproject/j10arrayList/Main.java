7856c17ba933b9fbf40153c87dde20da548ea33f
Ansh <joshiansh70@gmail.com>
2026-04-07 16:14:51 +0000

    Arraylist

---
+++ myfirstproject/j10arrayList/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int input;
+        String user = "";
+        ArrayList<Integer> list= new ArrayList<>();
+
+        Scanner scanner = new Scanner(System.in);
+        while (!(user.equals("exit"))) {
+            System.out.print("Input a no you want to add to the list: ");
+            input = scanner.nextInt();
+            System.out.print("If you don't want to continue type exit: ");
+            user = scanner.next();
+            list.add(input);
+        }
+        System.out.println(list);
+        scanner.close();
+    }
+}
