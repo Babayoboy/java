925b7b9aa1ab28956641c80c9f8ff4f16604d7a3
Ansh <joshiansh70@gmail.com>
2026-04-09 15:25:14 +0000

    error handling corrected

--- projects/songplayer/Main.java
+++ projects/songplayer/Main.java
@@ -1,10 +1,10 @@
 import java.io.File;
-import java.io.IOException;
 import java.util.Scanner;
-
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
